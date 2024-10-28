01e450592dbf7993f7a1286424faedcd4dddd5ba
coby111 <100744345+coby111@users.noreply.github.com>
2024-10-28 01:45:04 -0600

    Update firebaseConfig.ts

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -2,13 +2,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyAg44LXWZlHpnzsVbZ47TVrS1HavcyU_Uk",
-  authDomain: "auth-rick-cbe95.firebaseapp.com",
-  projectId: "auth-rick-cbe95",
-  storageBucket: "auth-rick-cbe95.appspot.com",
-  messagingSenderId: "216853438015",
-  appId: "1:216853438015:web:5089d656912ff9fb01e28a",
-  measurementId: "G-HCWT3253JX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
